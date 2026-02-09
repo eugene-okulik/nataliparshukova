9453454fb233f7f4a113dd12a67a5ae092018563
Natalia Parshukova <kettary_88@mail.ru>
2026-02-09 23:12:14 +0300

    Natali_parshukova_lesson_6

---
+++ homework/natali_parshukova/Lesson_6/lesson_6_1.py
@@ -0,0 +1,17 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+words = text.split()
+#print(words)
+fin_words = []
+for word in words:
+    if ',' in word:
+        word = word.replace(',', 'ing,')
+        #print (word)
+    elif '.' in word:
+        word = word.replace('.', 'ing.')
+        #print(word)
+    else:
+        word = f'{word}ing'
+        #print(word)
+    fin_words.append(word)
+#print(fin_words)
+print(' '.join(fin_words))
